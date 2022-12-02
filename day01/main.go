76c593adac84ea493c3fdcb1ab2b0da6d77998e7
Oskar Karlsson <78047648+Zairian@users.noreply.github.com>
2022-12-02 16:00:31 +0100

    Add print of both solutions

--- day01/main.go
+++ day01/main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(CalorieCount())
+	fmt.Printf("Elf with largest amount of calories is carrying: %v calories!\n", CalorieCount())
+	fmt.Printf("The three elves with largest amount of calories are carrying: %v calories!\n", CalorieCountThree())
 }
