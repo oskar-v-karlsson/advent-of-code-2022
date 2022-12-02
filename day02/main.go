5ea542cc4b63c20563af066d528c89680ce2208d
Oskar Karlsson <78047648+Zairian@users.noreply.github.com>
2022-12-02 20:27:57 +0100

    Add solution for true score

--- day02/main.go
+++ day02/main.go
@@ -7,5 +7,6 @@ import (
 )
 
 func main() {
-	fmt.Println(utils.CalculateScore())
+	fmt.Printf("Score without full strategy guide: %v points. \n", utils.CalculateScore())
+	fmt.Printf("Score with full strategy guide: %v points. \n", utils.CalculateTrueScore())
 }
--- day02/utils/rocks.go
+++ day02/utils/rocks.go
@@ -53,3 +53,56 @@ func checkResults(h []int) int {
 		return 0
 	}
 }
+
+func CalculateTrueScore() int {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+
+	totalScore := 0
+
+	for s.Scan() {
+		hands := strings.Split(s.Text(), " ")
+
+		// Converts hand characters to their respective scores
+		handValues := []int{scores[hands[0]], scores[hands[1]]}
+
+		switch hands[1] {
+		case "X":
+			totalScore += createLossHand(handValues)
+		case "Y":
+			totalScore += createDrawHand(handValues)
+			totalScore += 3
+		case "Z":
+			totalScore += createWinHand(handValues)
+			totalScore += 6
+		}
+	}
+	return totalScore
+}
+
+// Returns the number corresponding to winning hand
+func createWinHand(h []int) int {
+	if h[0] == 3 {
+		return 1
+	}
+	return h[0] + 1
+}
+
+// Returns the number corresponding to draw hand
+func createDrawHand(h []int) int {
+	return h[0]
+}
+
+// Returns the number corresponding to losing hand
+func createLossHand(h []int) int {
+	if h[0] == 1 {
+		return 3
+	}
+	return h[0] - 1
+}
