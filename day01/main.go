d064077cb96fc5b417d3fc9b0095b4fb53adb078
Oskar Karlsson <78047648+Zairian@users.noreply.github.com>
2022-12-02 16:25:18 +0100

    Change file structure

--- day01/main.go
+++ day01/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Zairian/advent-of-code-2022/utils"
+)
 
 func main() {
-	fmt.Printf("Elf with largest amount of calories is carrying: %v calories!\n", CalorieCount())
-	fmt.Printf("The three elves with largest amount of calories are carrying: %v calories!\n", CalorieCountThree())
+	fmt.Printf("Elf with largest amount of calories is carrying: %v calories!\n", utils.CalorieCount())
+	fmt.Printf("The three elves with largest amount of calories are carrying: %v calories!\n", utils.CalorieCountThree())
 }
