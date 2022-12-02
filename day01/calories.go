5ec4f9e15b23c1c7985923cc1998aea8c810beae
Oskar Karlsson <78047648+Zairian@users.noreply.github.com>
2022-12-02 16:00:19 +0100

    Add calorie count top three elves

--- day01/calories.go
+++ day01/calories.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -38,4 +39,41 @@ func CalorieCount() int {
 	}
 
 	return largest
+}
+
+// Count calories of individual elves and returns the sum of the largest three
+func CalorieCountThree() int {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+
+	var sortedCalories []int
+	sum := 0
+
+	for s.Scan() {
+		if s.Text() == "" {
+			sortedCalories = append(sortedCalories, sum)
+			sort.Sort(sort.Reverse(sort.IntSlice(sortedCalories)))
+			sum = 0
+			continue
+		}
+
+		i, err := strconv.Atoi(s.Text())
+		if err != nil {
+			log.Fatalln(err)
+		}
+		sum += i
+	}
+
+	largestThree := 0
+
+	for i := 0; i < 3; i++ {
+		largestThree += sortedCalories[i]
+	}
+	return largestThree
 }
