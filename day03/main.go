d07a915838ceb4d576ab7ae9e101f6834e053f79
Oskar Karlsson <78047648+Zairian@users.noreply.github.com>
2022-12-03 15:55:59 +0100

    Implement part1 priority calculator

--- day03/main.go
+++ day03/main.go
@@ -1,4 +1,11 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/Zairian/advent-of-code-2022/day03/utils"
+)
+
 func main() {
+	fmt.Printf("The sum of the priorities is: %v\n", utils.SumIdenticalPriorities())
 }
