005de0659cd2d6cf7833a64e8f9da5e2770faed8
Oskar Karlsson <78047648+Zairian@users.noreply.github.com>
2022-12-04 14:14:42 +0100

    Add print sum of partial overlaps

---
+++ day04/solutions/solution2.go
@@ -0,0 +1,11 @@
+package solutions
+
+import (
+	"fmt"
+
+	"github.com/Zairian/advent-of-code-2022/day04/utils"
+)
+
+func Solve2() {
+	fmt.Printf("Amount of partial overlaps: %v\n", utils.SumPartialOverlaps())
+}
