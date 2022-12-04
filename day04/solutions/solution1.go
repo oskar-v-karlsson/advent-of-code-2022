d83c655fc3319444a33e880bad2d795b9f334fe2
Oskar Karlsson <78047648+Zairian@users.noreply.github.com>
2022-12-04 13:11:44 +0100

    Add print of amount of full overlap

--- day04/solutions/solution1.go
+++ day04/solutions/solution1.go
@@ -1,1 +1,11 @@
 package solutions
+
+import (
+	"fmt"
+
+	"github.com/Zairian/advent-of-code-2022/day04/utils"
+)
+
+func Solve1() {
+	fmt.Printf("Amount of full overlaps: %v\n", utils.SumOverlaps())
+}
