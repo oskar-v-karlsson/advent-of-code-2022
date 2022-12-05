7bc92b3fe8408867f3f4a8e6940aa0065d875eda
Oskar Karlsson <78047648+Zairian@users.noreply.github.com>
2022-12-05 20:26:38 +0100

    Add crate moving logic

---
+++ day05/solver/crateMover.go
@@ -0,0 +1,89 @@
+package solver
+
+import (
+	"bufio"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/Zairian/advent-of-code-2022/utils"
+)
+
+func moveCrateMover9000() string {
+	crates := make([][]string, 9)
+
+	s := utils.GetScanner("input.txt")
+	defer utils.CloseOS()
+
+	parseCrates(s, crates)
+
+	for s.Scan() {
+		if s.Text() != "" {
+			number, from, to := parseActions(s.Text())
+			moveCrates(number, from, to, crates)
+		}
+	}
+	return getTopCrates(crates)
+}
+
+func moveCrateMover9001() string {
+	crates := make([][]string, 9)
+
+	s := utils.GetScanner("input.txt")
+	defer utils.CloseOS()
+
+	parseCrates(s, crates)
+
+	for s.Scan() {
+		if s.Text() != "" {
+			number, from, to := parseActions(s.Text())
+			moveMultipleCrates(number, from, to, crates)
+		}
+	}
+	return getTopCrates(crates)
+}
+
+func parseCrates(s *bufio.Scanner, crates [][]string) {
+	for i := 0; i < len(crates); i++ {
+		s.Scan()
+		crates[i] = strings.Split(s.Text(), ",")
+	}
+}
+
+// Returns the number and the indexes from where crates should be moved from and the index where it should be moved to
+func parseActions(action string) (int, int, int) {
+	re := regexp.MustCompile(`[-]?\d[\d,]*[\.]?[\d{2}]*`)
+	matches := re.FindAllString(action, -1)
+
+	number, _ := strconv.Atoi(matches[0])
+	from, _ := strconv.Atoi(matches[1])
+	to, _ := strconv.Atoi(matches[2])
+
+	return number, from - 1, to - 1
+}
+
+func moveCrates(number, from, to int, crates [][]string) {
+	for i := 0; i < number; i++ {
+		topCrateIndex := len(crates[from]) - 1
+		crates[to] = append(crates[to], crates[from][topCrateIndex])
+		// Remove moved element
+		crates[from] = crates[from][:len(crates[from])-1]
+	}
+}
+
+func moveMultipleCrates(number, from, to int, crates [][]string) {
+	for i := number; i > 0; i-- {
+		crateToBeMoved := len(crates[from]) - i
+		crates[to] = append(crates[to], crates[from][crateToBeMoved])
+	}
+	// Remove moved elements
+	crates[from] = crates[from][:len(crates[from])-number]
+}
+
+func getTopCrates(crates [][]string) string {
+	topCrates := ""
+	for _, stack := range crates {
+		topCrates += stack[len(stack)-1]
+	}
+	return topCrates
+}
