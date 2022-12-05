49bcb4d3a7251eb95b8123e4db98adb9acd10925
Oskar Karlsson <78047648+Zairian@users.noreply.github.com>
2022-12-05 20:26:45 +0100

    Add solution printers

---
+++ day05/solver/solutions.go
@@ -0,0 +1,11 @@
+package solver
+
+import "fmt"
+
+func Solve1() {
+	fmt.Printf("The top crates after moving the crates with the CrateMover9000 is: %s\n", moveCrateMover9000())
+}
+
+func Solve2() {
+	fmt.Printf("The top crates after moving the crates with the CrateMover9001 is: %s\n", moveCrateMover9001())
+}
