249b579fe67234c537266a26ca993162fab2ed63
Oskar Karlsson <78047648+Zairian@users.noreply.github.com>
2022-12-11 19:03:21 +0100

    Add print solutions

--- day11/solver/solutions.go
+++ day11/solver/solutions.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func Solve() {
 	fmt.Printf("The amount of monkey business is: %v\n", calcMonkeyBusiness())
+	fmt.Printf("The amount of monkey business is: %v\n", calcPanicedMonkeyBusiness())
 }
